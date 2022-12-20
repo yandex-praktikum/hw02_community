fb9be34397d28d5d30d857b4b558dda029dacf6c
Андрей Евтушенко <evtushenkoad@gmail.com>
2022-12-20 18:09:40 +0300

    Rew

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
+AMOUNT_OF_POSTS = 10
+
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.select_related('group')[:(AMOUNT_OF_POSTS)]
+    # posts = Post.objects.order_by('-pub_date')[:(AMOUNT_OF_POSTS)]
     context = {
         'posts': posts,
     }
@@ -12,7 +15,9 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.order_by('-pub_date')[:(AMOUNT_OF_POSTS)]
+    # posts = Post.objects.filter(group=group)
+    # .order_by('-pub_date')[:(AMOUNT_OF_POSTS)]
     context = {
         'group': group,
         'posts': posts,
