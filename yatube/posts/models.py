d76a6c3fe79ce0a6dedad4ed646913a0bb5fc6b9
Андрей Евтушенко <evtushenkoad@gmail.com>
2022-12-22 10:30:41 +0300

    22.12.2022

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -31,3 +31,6 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,7 +6,6 @@ AMOUNT_OF_POSTS = 10
 
 def index(request):
     posts = Post.objects.select_related('group')[:(AMOUNT_OF_POSTS)]
-    # posts = Post.objects.order_by('-pub_date')[:(AMOUNT_OF_POSTS)]
     context = {
         'posts': posts,
     }
@@ -16,8 +15,6 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.order_by('-pub_date')[:(AMOUNT_OF_POSTS)]
-    # posts = Post.objects.filter(group=group)
-    # .order_by('-pub_date')[:(AMOUNT_OF_POSTS)]
     context = {
         'group': group,
         'posts': posts,
